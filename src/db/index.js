6287fd30b60973424d47aa4943c72751df47d1bb
Ali <mrrizwanaali@gmail.com>
2025-07-19 14:43:46 +0500

    add api error and response

--- src/db/index.js
+++ src/db/index.js
@@ -2,15 +2,18 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`
+    );
 
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log("Error connecting to the database:", error);
-        process.exit(1);
-    }
-}
-
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection Failed:", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
