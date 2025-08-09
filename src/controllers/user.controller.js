3d02d4fe690e7bc2f38ec1af7083d3a555eb0ab3
ali <mrrizwanaali@gmail.com>
2025-08-09 14:46:11 +0500

    logout bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -120,7 +120,6 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
   };
 
   return res
@@ -144,27 +143,27 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(req.user._id,
-     {
-      $set:{
-        refreshToken: undefined
-      }
-     },
-     {
-      new: true,     // returned response got new values updated ones
-     }
-    )
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true, // returned response got new values updated ones
+    }
+  );
 
-    const options = {
+  const options = {
     httpOnly: true,
-    secure: true,
   };
 
   return res
     .status(200)
-    .clearCookie("accessToken",  options)
+    .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
-    .json(new ApiResponse(200, {} ,"Logout successful"));
-})
+    .json(new ApiResponse(200, {}, "Logout successful"));
+});
 
-export { registerUser, loginUser , logoutUser };
+export { registerUser, loginUser, logoutUser };
